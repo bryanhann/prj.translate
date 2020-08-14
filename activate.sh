b24330b1f926ffba2bec4945c34d88d95c085e70
Bryan Hann <nobody@nowhere>
2020-08-14 12:38:07 +0800

    Enable: create a persistent venv for self.

--- activate.sh
+++ activate.sh
@@ -1,8 +1,13 @@
 cd $(dirname $0)
 export ROOT=$PWD
-export PYTHONPATH=${ROOT}/lib
-source virtualenvwrapper.sh
-mktmpenv
-cd ${ROOT}
+venv_id_file=${ROOT}/.tmp_venv_id_file
+[[ -f ${venv_id_file} ]] || echo $RANDOM > ${venv_id_file}
+venv_id=$(cat $venv_id_file)
+echo $venv_id
+mkvirtualenv $venv_id
+workon $venv_id
+pip install --upgrade pip
 pip install -r ${ROOT}/.freeze.pip
-pytest
+
+export PYTHONPATH=${ROOT}/lib
+echo "type 'pytest' to test."
