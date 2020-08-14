ebcf094d68240505bc2c205feead68775209b9c0
Bryan Hann <nobody@nowhere>
2020-08-14 17:15:27 +0800

    Rely on bh0-workon rather than hooks.

--- activate.sh
+++ activate.sh
@@ -1,13 +1,15 @@
 cd $(dirname $0)
-export ROOT=$PWD
-venv_id_file=${ROOT}/.tmp_venv_id_file
-[[ -f ${venv_id_file} ]] || echo $RANDOM > ${venv_id_file}
-venv_id=$(cat $venv_id_file)
-echo $venv_id
-mkvirtualenv $venv_id
-workon $venv_id
-pip install --upgrade pip
-pip install -r ${ROOT}/.freeze.pip
-
-export PYTHONPATH=${ROOT}/lib
+export PYTHONPATH=${PWD}/lib
+#export ROOT=$PWD
+#export venv_id=$(md5 -r -s $PWD | cut -c1-6)
+#[[ -d $WORKON_HOME/$venv_id ]] || {
+#    echo press enter to installing venv
+#    read
+#    mkvirtualenv $venv_id
+#    echo press enter to upgrade
+#    read
+#    pip install --upgrade pip
+#    pip install -r ${ROOT}/.freeze.pip
+#}
+#workon $venv_id
 echo "type 'pytest' to test."
