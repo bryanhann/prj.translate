ddb49abb967f8668cea6b1cbc277fba3c9012f9e
Bryan Hann <nobody@nowhere>
2020-09-03 11:07:05 +0800

    Continue  move from using [datadir] to using [./_here]
    Removed dependancy on [datdir] in [test_dictionary.py]

--- project/dev/test_dictionary.py
+++ project/dev/test_dictionary.py
@@ -3,13 +3,13 @@ import prj.dictionary as UT # Under Test
 def textblock_4_seq(seq):
     return '\n'.join(map(str,list(seq)))
 
-def test__eGen1_4_dsfS(datadir, regtest):
-    dsfS = [ datadir["sample/dictionary.txt"], datadir["sample/bad-dictionary.txt"], ]
+def test__eGen1_4_dsfS(MFIX, regtest):
+    dsfS = [ MFIX.sample_good_dictionary, MFIX.sample_bad_dictionary ]
     eGen1 = UT.eGen1_4_dsfS( dsfS )
     regtest.write(textblock_4_seq(eGen1))
 
-def test__cGen1_4_eGen1(datadir,regtest):
-    dsfS = [ datadir["sample/dictionary.txt"], datadir["sample/bad-dictionary.txt"], ]
+def test__cGen1_4_eGen1(MFIX,regtest):
+    dsfS = [ MFIX.sample_good_dictionary, MFIX.sample_bad_dictionary ]
     eGen1 = UT.eGen1_4_dsfS(dsfS)
     cGen1 = UT.cGen1_4_eGen1(eGen1)
     regtest.write( textblock_4_seq(cGen1))
