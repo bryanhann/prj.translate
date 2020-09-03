6aeb405e2a4f0f3cdc56775e535a1d6f7a191456
Bryan Hann <nobody@nowhere>
2020-09-03 09:49:15 +0800

    Begin move from using [datadir] to using [./_here]

--- project/dev/_conftest/MFIX/__init__.py
+++ project/dev/_conftest/MFIX/__init__.py
@@ -8,18 +8,22 @@ import tempfile
 import _here
 
 @pytest.fixture(scope='module')
-def mfix_ripT(mfix_ripF):
-    return mfix_ripF.read_text(encoding=None)
+def mfix_sample_passage():          return _here.DATADIR / 'sample/passage.txt'
 
 @pytest.fixture(scope='module')
-def mfix_ripF():
-    return _here.DATADIR / 'TheTitle.src'
+def mfix_sample_good_dictionary():  return _here.DATADIR / 'sample/dictionary.txt'
 
 @pytest.fixture(scope='module')
-def mfix_ripT(mfix_ripF):
-    return mfix_ripF.read_text(encoding=None)
+def mfix_sample_bad_dictionary():   return _here.DATADIR / 'sample/bad-dictionar.txt'
 
 @pytest.fixture(scope='module')
-def mfix_temp():
-    return pathlib.Path(tempfile.mkdtemp())
+def mfix_ripF():                    return _here.DATADIR / 'TheTitle.src'
 
+@pytest.fixture(scope='module')
+def mfix_temp():                    return pathlib.Path(tempfile.mkdtemp())
+
+@pytest.fixture(scope='module')
+def mfix_ripT(mfix_ripF):           return mfix_ripF.read_text(encoding=None)
+
+@pytest.fixture(scope='module')
+def mfix_ripT(mfix_ripF):           return mfix_ripF.read_text(encoding=None)
--- project/dev/conftest.py
+++ project/dev/conftest.py
@@ -1,18 +1,14 @@
 from _conftest import *
-
 import prj.util  as __U
 import _tools._formatter as __F
 import prj.couplet as __C
-
-PASSAGE_PATH = "sample/passage.txt"
+@pytest.fixture
+def LINES(MFIX):
+    return __U.lines_4_path(MFIX.sample_passage)
 
 @pytest.fixture
-def LINES(datadir):
-    return __U.lines_4_path(datadir[PASSAGE_PATH])
-
-@pytest.fixture
-def TEXT(datadir):
-    return __U.text_4_path(datadir[PASSAGE_PATH])
+def TEXT(MFIX):
+    return __U.text_4_path(MFIX.sample_passage)
 
 @pytest.fixture
 def RAWCOUPLETS(LINES):
