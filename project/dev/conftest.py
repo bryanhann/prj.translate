d56b165609c941d56f80c09d535e0384a2ce7ab1
Bryan Hann <nobody@nowhere>
2020-09-03 09:24:06 +0800

    Refactor tests with slight change in regression.

--- project/dev/conftest.py
+++ project/dev/conftest.py
@@ -1,2 +1,27 @@
 from _conftest import *
 
+import prj.util  as __U
+import _tools._formatter as __F
+import prj.couplet as __C
+
+PASSAGE_PATH = "sample/passage.txt"
+
+@pytest.fixture
+def LINES(datadir):
+    return __U.lines_4_path(datadir[PASSAGE_PATH])
+
+@pytest.fixture
+def TEXT(datadir):
+    return __U.text_4_path(datadir[PASSAGE_PATH])
+
+@pytest.fixture
+def RAWCOUPLETS(LINES):
+    return __C.rawcouplets_4_lines(LINES)
+
+@pytest.fixture
+def COUPLETS(RAWCOUPLETS):
+    return map( __C.couplet_4_rawcouplet, RAWCOUPLETS)
+
+@pytest.fixture
+def FORMAT(regtest):
+    return __F.Formatter( stdout=regtest )
--- project/dev/test_functions.py
+++ project/dev/test_functions.py
@@ -1,33 +1,4 @@
-import sys
-import pytest
-
-import prj.util  as __U
-import _tools._formatter as __F
-import prj.couplet as __C
 import prj.blips as __B
-
-PASSAGE_PATH = "sample/passage.txt"
-
-@pytest.fixture
-def LINES(datadir):
-    return __U.lines_4_path(datadir[PASSAGE_PATH])
-
-@pytest.fixture
-def TEXT(datadir):
-    return __U.text_4_path(datadir[PASSAGE_PATH])
-
-@pytest.fixture
-def RAWCOUPLETS(LINES):
-    return __C.rawcouplets_4_lines(LINES)
-
-@pytest.fixture
-def COUPLETS(RAWCOUPLETS):
-    return map( __C.couplet_4_rawcouplet, RAWCOUPLETS)
-
-@pytest.fixture
-def FORMAT(regtest):
-    return __F.Formatter( stdout=regtest )
-
 
 def test__the_sample_passage_startswith_ETAG(TEXT):
     assert TEXT.startswith('<E>')
