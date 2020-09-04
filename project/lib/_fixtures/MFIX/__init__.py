884b20a1ded19bd0103e6c236d61156e1a7548d2
Bryan Hann <nobody@nowhere>
2020-09-04 10:56:55 +0800

    Refactor fixture [MFIX]: factor out [xRIP].

--- project/lib/_fixtures/MFIX/__init__.py
+++ project/lib/_fixtures/MFIX/__init__.py
@@ -1,25 +1,18 @@
+import pytest
+import pathlib
+import tempfile
+
+@pytest.fixture(scope='module')
+def mfix_temp(): return pathlib.Path(tempfile.mkdtemp())
+
+from _fixtures.MFIX.SAMPLE import *
+from _fixtures.MFIX.xRIP import *
 
 from _fixtures.MFIX.message import moo_the_module_test_message
 from _fixtures.MFIX.message import moo_the_module_test_message as foobar_missive
 
-import pytest
-import pathlib
-import tempfile
-import _here
 
-from _fixtures.MFIX.SAMPLE import *
 
-@pytest.fixture(scope='module')
-def mfix_ripF():                    return _here.DATADIR / 'TheTitle.src'
-
-@pytest.fixture(scope='module')
-def mfix_temp():                    return pathlib.Path(tempfile.mkdtemp())
-
-@pytest.fixture(scope='module')
-def mfix_ripT(mfix_ripF):           return mfix_ripF.read_text(encoding=None)
-
-@pytest.fixture(scope='module')
-def mfix_ripT(mfix_ripF):           return mfix_ripF.read_text(encoding=None)
 
 
 
---
+++ project/lib/_fixtures/MFIX/xRIP.py
@@ -0,0 +1,12 @@
+import pytest
+import _here
+
+@pytest.fixture(scope='module')
+def mfix_ripF():                    return _here.DATADIR / 'TheTitle.src'
+
+@pytest.fixture(scope='module')
+def mfix_ripT(mfix_ripF):           return mfix_ripF.read_text(encoding=None)
+
+@pytest.fixture(scope='module')
+def mfix_ripT(mfix_ripF):           return mfix_ripF.read_text(encoding=None)
+
