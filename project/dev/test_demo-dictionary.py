b4f0b6dfb4fe3d1df7509c0ef4eecf4889d63bed
Bryan Hann <nobody@nowhere>
2020-09-04 08:52:47 +0800

    Move demo scripts from lib/prj to lib/demos

--- project/dev/test_demo-dictionary.py
+++ project/dev/test_demo-dictionary.py
@@ -1,6 +1,6 @@
 from prj.ezrun import ezrun
 
-TEMPLATE = 'python -m prj.demo-dictionary %s'
+TEMPLATE = 'python -m demos.demo-dictionary %s'
 
 def test_bad_dict  (MFIX,FFIX): FFIX.ezrun_with_capture( TEMPLATE % MFIX.sample_bad_dictionary   )
 def test_good_dict (MFIX,FFIX): FFIX.ezrun_with_capture( TEMPLATE % MFIX.sample_good_dictionary   )
