2c2ee8e5a7dc2247c0dd14b67e5c2478f57c4abc
Bryan Hann <nobody@nowhere>
2020-09-05 11:47:33 +0800

    BUGFIX: Refactor [testdata].
    Name eclipse. The name [testdata.sample] could refer to either
    to the name [sample] in [testdata/__init__.py], or to the package
    [testdata/sample.py].
    
    Solution: remove all code from [testdata/__init__.py] and change
    file [testdata/sample.py] to package [testdata/sample/]. So too
    with [standard].

---
+++ project/lib/testdata/sample/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+__nugget__=Path(__file__).parent/'__nugget__'
+
+passage          = __nugget__ / 'passage.txt'
+good_dictionary  = __nugget__ / 'dictionary.txt'
+bad_dictionary   = __nugget__ / 'bad-dictionary.txt'
+dictionaries     = [good_dictionary, bad_dictionary]
---
+++ project/lib/testdata/standard/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+__nugget__=Path(__file__).parent/'__nugget__'
+
+ripfile=__nugget__/'TheTitle.src'
+
+
