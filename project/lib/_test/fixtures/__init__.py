47e73e4be0bdf2f3d2ff7cf9d9e9439692b12005
Bryan Hann <nobody@nowhere>
2020-09-05 08:44:01 +0800

    Add test fixture [REGRESS]

--- project/lib/_test/fixtures/__init__.py
+++ project/lib/_test/fixtures/__init__.py
@@ -41,3 +41,11 @@ from _test.fixtures.sample import *
 @pytest.fixture
 def SAMPLE(lines,text,rawcouplets,couplets): return FB( lines,text,rawcouplets,couplets, 'LINES TEXT RAWCOUPLETS COUPLETS')
 
+@pytest.fixture
+def REGRESS(regtest):
+    def textblock_4_seq(seq): return '\n'.join(map(str,list(seq)))
+    class Namespace: pass
+    X=Namespace()
+    X.regtest = regtest
+    X.blockwrite = lambda seq: regtest.write(textblock_4_seq(seq))
+    return X
