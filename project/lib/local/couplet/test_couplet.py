3c2b9aeba08e4a478fbdf8140e9a66d179963043
Bryan Hann <nobody@nowhere>
2020-09-06 17:06:12 +0800

    Replace fixture [REGRESS] with fixture [THIS] in tests.

--- project/lib/local/couplet/test_couplet.py
+++ project/lib/local/couplet/test_couplet.py
@@ -1,8 +1,5 @@
 from local.couplet import plips_4_couplets as FN
-
-def test_plips         (THESAMPLE_couplets,REGRESS): REGRESS.formatter.desc( 'the plips.')              .seq( FN(THESAMPLE_couplets) ).run()
-def test_lines         (THESAMPLE_lines,REGRESS): REGRESS.formatter.desc( 'the lines.' )                .seq( THESAMPLE_lines         ).run()
-def test_rawcouplets   (THESAMPLE_rawcouplets,REGRESS): REGRESS.formatter.desc( 'the raw couplets.' )   .seq( THESAMPLE_rawcouplets   ).run()
-def test_couplets      (THESAMPLE_couplets,REGRESS): REGRESS.formatter.desc( 'the couplets.' )          .seq( THESAMPLE_couplets      ).run()
-
-
+def test_regress_THESAMPLE_lines        (THIS, THESAMPLE_lines       ): THIS(THESAMPLE_lines,        show=0)
+def test_regress_THESAMPLE_rawcouplets  (THIS, THESAMPLE_rawcouplets ): THIS(THESAMPLE_rawcouplets,  show=0)
+def test_regress_THESAMPLE_couplets     (THIS, THESAMPLE_couplets    ): THIS(THESAMPLE_couplets,     show=0)
+def test__plips                         (THIS, THESAMPLE_couplets    ): THIS(FN(THESAMPLE_couplets), show=0)
--- project/lib/testfixtures/__init__.py
+++ project/lib/testfixtures/__init__.py
@@ -41,14 +41,4 @@ def DUMP(dump,dumpfiles,ezrun): return FB( dump,dumpfiles,ezrun, 'dump dumpfiles
 
 
 from testfixtures.regress import REGRESS
-#@pytest.fixture
-#def REGRESS(regtest):
-#    import _tools._formatter
-#    def textblock_4_seq(seq): return '\n'.join(map(str,list(seq)))
-#    class Namespace: pass
-#    X=Namespace()
-#    X.regtest = regtest
-#    X.blockwrite = lambda seq: regtest.write(textblock_4_seq(seq))
-#    X.formatter = _tools._formatter.Formatter( stdout=regtest )
-#    return X
 
