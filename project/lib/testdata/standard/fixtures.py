67cb983244e2b21d4140002be9c7295dbefa6d6a
Bryan Hann <nobody@nowhere>
2020-09-06 19:30:04 +0800

    Cosmetic cleanup.

--- project/lib/testdata/standard/fixtures.py
+++ project/lib/testdata/standard/fixtures.py
@@ -4,6 +4,7 @@ import testdata
 import local.x4rip
 import local.dictionary
 import local.blips
+
 @pytest.fixture(scope='module')
 def THE_pip(THE_pip2D):         return THE_pip2D[(0,0)]
 @pytest.fixture(scope='module')
@@ -14,14 +15,9 @@ def THE_rip():                  return testdata.standard.ripfile.read_text()
 def THE_dictTuples():           return local.dictionary.dictTuples_4_dictFiles( testdata.standard.dictionaries)
 @pytest.fixture(scope='module')
 def THE_dikt(THE_dictTuples):   return local.dictionary.dict_4_dictTuples(THE_dictTuples )
-
 @pytest.fixture(scope='module')
 def THE_plips(THE_pip):         return local.blips.plips_4_pip(THE_pip)
 
-#from local.blips    import plips_4_pip
-#from the_PIP        import pip
-
-#plips = plips_4_pip(pip)
 
 
 
