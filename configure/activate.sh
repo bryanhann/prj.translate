f0c48d56651b22867f4e9bce74788ec8c6a0fde8
Bryan Hann <nobody@nowhere>
2020-09-30 11:06:42 +0800

    Provide more shell environment variables.

--- configure/activate.sh
+++ configure/activate.sh
@@ -1,4 +1,9 @@
 export PYTHONPATH=
 export PYTHONPATH=$PYTHONPATH:${PWD}/project/lib
 export PYTHONPATH=$PYTHONPATH:${PWD}/project/bin
-alias pd="python -m" 
+export PRJ_ROOT=$PWD
+export PRJ_CODE=$PWD/project
+export PRJ_dict=$PWD/submodules/dict.repo/dict.txt
+export PRJ_eng=$PWD/submodules/units.repo/C1_20200912_1/eng.txt
+export PRJ_chi=$PWD/submodules/units.repo/C1_20200912_1/chi.txt
+git submodule update --init
