94ce043333c8136fce5488a003f5b63f566d90cc
Bryan Hann <nobody@nowhere>
2020-09-30 16:40:09 +0800

    Use master repository index.

--- configure/activate.sh
+++ configure/activate.sh
@@ -7,3 +7,7 @@ export PRJ_dict=$PWD/submodules/dict.repo/dict.txt
 export PRJ_eng=$PWD/submodules/units.repo/C1_20200912_1/eng.txt
 export PRJ_chi=$PWD/submodules/units.repo/C1_20200912_1/chi.txt
 git submodule update --init
+export MASTER_INDEX_URL=https://github.com/bryanhann/master-index
+export MASTER_INDEX_DST=$PWD/__INDEX__
+git clone $MASTER_INDEX_URL $MASTER_INDEX_DST
+
